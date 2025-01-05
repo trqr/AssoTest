11f06e8d966f7d0392e0127c2bcccc7430cfc35b
TrqR <tristanqr@gmail.com>
2025-01-05 11:27:35 +0100

    Box style update

--- data/subscriber.js
+++ data/subscriber.js
@@ -20,15 +20,18 @@ document.querySelector('.submit-button').addEventListener('click', (event) => {
     saveToStorage();
 
     document.querySelector('.subscription-form').remove();
-    document.querySelector('body').innerHTML += `<div class="subscription-review box"><h1>Thank you for your subscription!</h1>
-        <div class="member-details">
-            <div class="detail"><strong>Name:</strong> ${member.name}</div>
-            <div class="detail"><strong>Email:</strong> ${member.email}</div>
-            <div class="detail"><strong>Phone:</strong> ${member.phone}</div>
-            <div class="detail"><strong>Membership:</strong> ${member.membership}</div>
-            <div class="detail"><strong>Payment:</strong> ${member.payment}</div>
+    document.querySelector('body').innerHTML +=
+        `<div class="subscription-review box">
+            <div class="box-info"><h1>Thank you for your subscription!</h1>
+                <div class="member-details">
+                    <div class="detail"><strong>Name:</strong> ${member.name}</div>
+                    <div class="detail"><strong>Email:</strong> ${member.email}</div>
+                    <div class="detail"><strong>Phone:</strong> ${member.phone}</div>
+                    <div class="detail"><strong>Membership:</strong> ${member.membership}</div>
+                    <div class="detail"><strong>Payment:</strong> ${member.payment}</div>
+                </div>
+            </div>
         </div>
-    </div>
-`;
+        `;
 });
 
